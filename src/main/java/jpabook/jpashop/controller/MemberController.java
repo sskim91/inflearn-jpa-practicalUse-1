ffab82cf117d3c30e6d128450c7b3bce6d126494
sskim91 <tjdtjq91@gmail.com>
2020-12-13 22:33:50 +0900

    feat: 회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -39,6 +40,15 @@ public class MemberController {
         member.setName(form.getName());
         member.setAddress(address);
 
+        memberService.join(member);
+
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
