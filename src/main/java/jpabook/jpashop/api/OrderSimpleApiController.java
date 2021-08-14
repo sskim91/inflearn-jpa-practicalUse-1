6badd9ae61f44979b2d0baf6e48de0a733a36344
sskim91 <tjdtjq91@gmail.com>
2021-08-14 17:08:28 +0900

    API 개발과 성능 최적화 - 간단한 주문 조회 V3: 엔티티를 DTO로 변환 페치 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -15,6 +15,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 /**
  * Created by sskim on 2021/08/14
  * Github : http://github.com/sskim91
@@ -59,6 +61,20 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
 
         return new Result(collect);
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함)
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public Result<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return new Result(result);
     }
 
     @Data
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -28,8 +28,8 @@ public class OrderRepository {
     public List<Order> findAll(OrderSearch orderSearch) {
 
         return em.createQuery("select  o from Order o join o.member m" +
-                " where  o.status = :status " +
-                " and m.name like :name", Order.class)
+                        " where  o.status = :status " +
+                        " and m.name like :name", Order.class)
                 .setParameter("status", orderSearch.getOrderStatus())
                 .setParameter("name", orderSearch.getMemberName())
                 .setMaxResults(1000)    //최대 1000건
@@ -101,5 +101,15 @@ public class OrderRepository {
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
         return query.getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        String query = "select o from Order o" +
+                " join fetch o.member m " +
+                " join fetch o.delivery d";
+
+        List<Order> resultList = em.createQuery(query, Order.class)
+                .getResultList();
+        return resultList;
     }
 }
